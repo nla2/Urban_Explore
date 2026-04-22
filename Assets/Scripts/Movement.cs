0801c7c83eb31a1055dce18f8aae2d53d8d3d3e3
Nathan Ashworth <nathan.lee.ashworth@gmail.com>
2026-04-22 17:11:32 +0200

    modified the movement and turning code so that its less busy

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,9 +4,16 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     public float MovementSpeed = 1;
+    Vector2 moveInput;
     public float JumpHeight = 1;
-    public float RotationSpeed = 100;
+    public float RotationSpeed = 50;
+    Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    void Awake()
+    {
+      rb = GetComponent<Rigidbody>();
+    }
     void Start()
     {
         
@@ -15,45 +22,68 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Move();
-        Look();
+        // OnMove();
+        // Look();
     }
 
-    void Move()
+    void FixedUpdate()
     {
-       if (Input.GetKey(KeyCode.W))
-        {
-            Debug.Log("W key was pressed");
-            transform.Translate(Vector3.forward * MovementSpeed * Time.deltaTime);
-        } 
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            Debug.Log("S key was pressed");
-            transform.Translate(Vector3.back * MovementSpeed * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            Debug.Log("A key was pressed");
-            transform.Translate(Vector3.left * MovementSpeed * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            Debug.Log("D key was pressed");
-            transform.Translate(Vector3.right * MovementSpeed * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            Debug.Log("i jumped");
-            transform.Translate(Vector3.up * JumpHeight * Time.deltaTime);
-        }
+        moveLogic();
     }
-    void Look()
+
+    void OnMove(InputValue value)
     {
-        float mouseX = Input.GetAxis("Mouse X");
+        moveInput = value.Get<Vector2>();
+    }
+    void OnLook(InputValue value)
+    {
+        Vector2 lookInput = value.Get<Vector3>();
+        float mouseX = lookInput.x;
         transform.Rotate(Vector3.up * mouseX * RotationSpeed * Time.deltaTime);
     }
+
+    void moveLogic()
+    {
+        Vector3 moveVector = new Vector3(moveInput.x, 0, moveInput.y) * MovementSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + moveVector);
+    }
+    
+
+
+    //    if (Input.GetKey(KeyCode.W))
+    //     {
+    //         Debug.Log("W key was pressed");
+    //         transform.Translate(Vector3.forward * MovementSpeed * Time.deltaTime);
+    //     } 
+
+    //     if (Input.GetKey(KeyCode.S))
+    //     {
+    //         Debug.Log("S key was pressed");
+    //         transform.Translate(Vector3.back * MovementSpeed * Time.deltaTime);
+    //     }
+
+    //     if (Input.GetKey(KeyCode.A))
+    //     {
+    //         Debug.Log("A key was pressed");
+    //         transform.Translate(Vector3.left * MovementSpeed * Time.deltaTime);
+    //     }
+
+    //     if (Input.GetKey(KeyCode.D))
+    //     {
+    //         Debug.Log("D key was pressed");
+    //         transform.Translate(Vector3.right * MovementSpeed * Time.deltaTime);
+    //     }
+
+    //     if (Input.GetKey(KeyCode.Space))
+    //     {
+    //         Debug.Log("i jumped");
+    //         transform.Translate(Vector3.up * JumpHeight * Time.deltaTime);
+    //     }
+
+    
+    // void Look()
+    // {
+    //     float mouseX = Input.GetAxis("Mouse X");
+    //     transform.Rotate(Vector3.up * mouseX * RotationSpeed * Time.deltaTime);
+    // }
 }
